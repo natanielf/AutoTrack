cb6d4e023bc558d155a458201ca2b71cfa5b914b
natanielf <natanielfarzan@gmail.com>
2022-07-01 11:37:54 -0700

    Continue building out app UI

--- app/src/main/java/com/natanielfarzan/autotrack/ui/AutoTrackApp.kt
+++ app/src/main/java/com/natanielfarzan/autotrack/ui/AutoTrackApp.kt
@@ -3,6 +3,7 @@ package com.natanielfarzan.autotrack.ui
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
@@ -13,55 +14,37 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.natanielfarzan.autotrack.ui.theme.AutoTrackTheme
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AutoTrackApp() {
     AutoTrackTheme {
-        Scaffold(
-            topBar = {
-                SmallTopAppBar(
-                    title = { Text("Expenses") },
-                    navigationIcon = {
-                        IconButton(onClick = { /*TODO*/ }) {
-                            Icon(
-                                imageVector = Icons.Filled.Menu,
-                                contentDescription = null
-                            )
-                        }
-                    },
-                    actions = {
-                        IconButton(onClick = { /*TODO*/ }) {
-                            Icon(
-                                imageVector = Icons.Filled.Favorite,
-                                contentDescription = null
-                            )
-                        }
-                    },
-                )
-            },
-            content = { innerPadding ->
-                LazyColumn(contentPadding = innerPadding) {
-                    items(count = 100) {
-                        Box(
-                            Modifier
-                                .fillMaxWidth()
-                                .height(50.dp)
-                        )
-                    }
-                }
-            }
-        )
-
+        AutoTrackAppPreview()
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Preview
 @Composable
 fun AutoTrackAppPreview() {
-    AutoTrackTheme {
-       AutoTrackAppBar()
-//       AutoTrackFloatingActionButton()
-    }
+    Scaffold(
+        topBar = { AutoTrackAppBar() },
+        floatingActionButtonPosition = FabPosition.End,
+        floatingActionButton = { AutoTrackFloatingActionButton() },
+        content = { innerPadding ->
+            LazyColumn(contentPadding = innerPadding) {
+                items(count = 100) {
+                    Box(
+                        Modifier
+                            .fillMaxWidth()
+                            .height(50.dp)
+                            .padding(10.dp)
+                    ) {
+                        Text(text = "Item $it")
+                    }
+                }
+            }
+        },
+        bottomBar = { AutoTrackNavigationBar() }
+    )
 }
 
 @Composable
@@ -89,7 +72,25 @@ fun AutoTrackAppBar() {
 
 @Composable
 fun AutoTrackFloatingActionButton() {
-    SmallFloatingActionButton(onClick = { /* TODO: Create new expense */ }) {
+    FloatingActionButton(onClick = { /* TODO: Create new expense */ }) {
         Icon(Icons.Filled.Add, contentDescription = "Create expense")
+    }
+}
+
+@Composable
+fun AutoTrackNavigationBar(modifier: Modifier = Modifier) {
+    NavigationBar(modifier) {
+        NavigationBarItem(
+            icon = { Icon(Icons.Default.Home, contentDescription = null) },
+            label = { Text("Expenses") },
+            selected = true,
+            onClick = { /*TODO*/ }
+        )
+        NavigationBarItem(
+            icon = { Icon(Icons.Default.Info, contentDescription = null) },
+            label = { Text("Data") },
+            selected = false,
+            onClick = { /*TODO*/ }
+        )
     }
 }
